a39685de334ed574c99044808b6e68646d62a43e
April Horne <asukablaze@gmail.com>
2017-11-19 01:11:13 -0500

    added skeleton ogl support

--- src/charviewform.h
+++ src/charviewform.h
@@ -15,8 +15,25 @@ public:
     explicit CharViewForm(QWidget *parent = 0);
     ~CharViewForm();
 
+protected:
+    void InitGL();
+    void PaintGL();
+    void ResizeGL(int width, int height);
+
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
 private:
     Ui::CharViewForm *ui;
+    void DrawScene();
+    int xRot;
+    int yRot;
+    int zRot;
+
+signals:
+    void xRotationChanged(int angle);
+    void yRotationChanged(int angle);
+    void zRotationChanged(int angle);
 };
 
 #endif // CHARVIEWFORM_H
